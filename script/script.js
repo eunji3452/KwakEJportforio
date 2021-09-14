70821a9f8a77f7c60998c74ae0bade7c9fb8b7ae
eunji3452 <82145698+eunji3452@users.noreply.github.com>
2021-09-14 13:13:45 +0900

    pf javascript

--- script/script.js
+++ script/script.js
@@ -28,3 +28,36 @@
       }
     });
   });
+
+    const more_btn1 = document.querySelector('#more_btn1');
+    const moreconts1 = document.querySelector('#moreconts1');
+
+    const more_btn2 = document.querySelector('#more_btn2');
+    const moreconts2 = document.querySelector('#moreconts2');
+
+    const more_btn3 = document.querySelector('#more_btn3');
+    const moreconts3 = document.querySelector('#moreconts3');
+
+    function toggleMenu1() {
+      moreconts1.classList.toggle('active1');
+    }
+    more_btn1.addEventListener('click', toggleMenu1);
+
+    function toggleMenu2() {
+      moreconts2.classList.toggle('active2');
+    }
+    more_btn2.addEventListener('click', toggleMenu2);
+
+    function toggleMenu3() {
+      moreconts3.classList.toggle('active3');
+    }
+    more_btn3.addEventListener('click', toggleMenu3);
+
+  
+
+    
+    
+    
+
+    
+
